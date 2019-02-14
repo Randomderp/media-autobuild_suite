079e6844d04d333cfe3f8d5fd51d856c94a604f1
Christopher Degawa <ccom@randomderp.com>
2019-02-14 13:07:25 -0600

    Modified write-log

--- media-autobuild_suite.ps1
+++ media-autobuild_suite.ps1
@@ -471,6 +471,8 @@ function Write-BaseFolders ([int]$bit) {
         "bin bin-audio bin-global bin-video etc include lib\pkgconfig share".Split(' ') | ForEach-Object {New-Item -ItemType Directory $PSScriptRoot\local$bit\$_ -ErrorAction Ignore | Out-Null}
     }
 }
+if ($build64 -eq "yes") {Write-BaseFolders -bit 64}
+if ($build32 -eq "yes") {Write-BaseFolders -bit 32}
 
 function Write-Fstab {
     Write-Output "$("-"*60)`n`n- write fstab mount file`n`n$("-"*60)"
@@ -480,24 +482,47 @@ function Write-Fstab {
     New-Item -Force -ItemType File -Path $fstab -Value $fstabtext | Out-Null
 }
 function Write-Log ([string]$logfile, [switch]$Script, [ScriptBlock]$ScriptBlock, [switch]$commandbash, [string]$BashCommand) {
+    function Write-Transcript {
+        try {Stop-Transcript | Out-Null} catch [System.InvalidOperationException] {}
+        if (Select-String -Path $LogFile -SimpleMatch -Pattern '**********************' -Quiet) {
+            $linenumber = (Select-String -Path $LogFile -SimpleMatch -Pattern '**********************').LineNumber
+            $transcriptContent = Get-Content -Path $LogFile | Select-Object -Index ($linenumber[1]..($linenumber[$linenumber.Length - 2] - 2))
+            Set-Content -Force -Path $LogFile -Value $transcriptContent
+        }
+    }
     try {
         Start-Transcript -Force -Path $logfile | Out-Null
-        if ($Script) {
-            &$ScriptBlock
-        }
-        if ($commandbash) {
-            &$bash @($BashCommand.Split(' '))
+        if ($Script) {&$ScriptBlock}
+        Remove-Item -Force -Path "$build\compilation_failed", "$build\fail_comp" -ErrorAction Ignore
+        if ($commandbash) {&$bash @($BashCommand.Split(' '))}
+        if (Test-Path "$build\compilation_failed") {
+            Write-Transcript
+            $compilefail = Get-Content -Path $build\compilation_failed
+            $env:reason = $compilefail[1]
+            $env:operation = $compilefail[2]
+            New-Item -Force -ItemType File -Path "$build\fail_comp" -Value $(
+                "while read line; do declare -x `"`$line`"; done < /build/fail.var`n" +
+                "source /build/media-suite_helper.sh`n" +
+                "cd `$(head -n 1 /build/compilation_failed)`n" +
+                "if [[ `$logging = y ]]; then`n" +
+                "echo `"Likely error:`"`n" +
+                "tail `"ab-suite.`${operation}.log`"`n" +
+                "echo `"`${red}`$reason failed. Check `$(pwd -W)/ab-suite.`$operation.log`${reset}`"`n" +
+                "fi`n" +
+                "echo `"`${red}This is required for other packages, so this script will exit.`${reset}`"`n" +
+                "zip_logs`n" +
+                "echo `"Make sure the suite is up-to-date before reporting an issue. It might've been fixed already.`"`n" +
+                "do_prompt `"Try running the build again at a later time.`"") | Out-Null
+            Start-Process -NoNewWindow -Wait -FilePath $bash -ArgumentList ("-l /build/fail_comp").Split(' ')
+            Remove-Item -Force -Path $build\compilation_failed, $build\fail_comp
         }
     } catch {
         Write-Output "Stopping log and exiting"
     } finally {
-        Stop-Transcript | Out-Null
-        Get-ChildItem -File -Include *.log -Path $build | ForEach-Object {Set-Content -Path $_.fulllname -Value $(Get-Content $_.fullname | Where-Object {$_ -notmatch "Username|RunAs|Machine|Host Application"})}
+        Write-Transcript
     }
 }
 
-if ($build64 -eq "yes") {Write-BaseFolders -bit 64}
-if ($build32 -eq "yes") {Write-BaseFolders -bit 32}
 if (!(Test-Path $PSScriptRoot\mintty.lnk)) {
     Set-Location $msys2Path
     if ($msys2 -eq "msys32") {
